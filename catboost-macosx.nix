ec76d6551cf5112f335cbd018657b270053e8445
Carman Fu <futarimiti@163.com>
2025-06-13 00:35:30 +0100

    use fetchPypi in place of fetchurl to fetch catboost macosx wheel

--- catboost-macosx.nix
+++ catboost-macosx.nix
@@ -7,15 +7,18 @@
 }:
 let
   python = pkgs.python39;
-  src = pkgs.fetchurl {
-    url = "https://files.pythonhosted.org/packages/63/ae/70fc292ccf056a9945e0f4591c29bbc5f018967e06eeb3f5a9449e0defff/catboost-1.2.5-cp39-cp39-macosx_11_0_universal2.whl";
-    sha256 = "ed9521ce617f370dd7f8214f6b5ea2cdb2bfb47a2be0fb19a03b7c9d4399b4d1";
-  };
 in
-python.pkgs.buildPythonPackage {
+python.pkgs.buildPythonPackage rec {
   pname = "catboost";
   version = "1.2.5";
-  inherit src;
+  src = pkgs.fetchPypi {
+    inherit pname version;
+    format = "wheel";
+    platform = "macosx_11_0_universal2";
+    python = "cp39";
+    abi = "cp39";
+    sha256 = "ed9521ce617f370dd7f8214f6b5ea2cdb2bfb47a2be0fb19a03b7c9d4399b4d1";
+  };
   format = "other";
   dontUnpack = true;
   installPhase = ''
