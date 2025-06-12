17d43def210504190dd14501f555a818103bdea6
Carman Fu <futarimiti@163.com>
2025-06-12 01:07:06 +0100

    use python 3.9

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Accurate folding topologies predictor for DNA G4 in potassium (K+) buffer, based on putative quadruplex sequences";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05"; # python39 support removed onwards
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -15,20 +15,36 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pythonVersion = "311";
-        python = pkgs."python${pythonVersion}";
+        old-scikit-learn-overlay = self: super: {
+          pythonPackagesExtensions = super.pythonPackagesExtensions ++ [
+            (pyself: pysuper: {
+              scikit-learn = pysuper.scikit-learn.overridePythonAttrs (old: rec {
+                version = "1.0.2";
+                src = pysuper.fetchPypi {
+                  pname = old.pname;
+                  inherit version;
+                  sha256 = "sha256-tYcJWaVIS2FPJtMcpMF1JLGwMXUiGZ3JhcO0JW4DB2c=";
+                };
+              });
+            })
+          ];
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ old-scikit-learn-overlay ];
+        };
+        python = pkgs.python39;
       in
       {
         # packages = rec {
-        #   {{cookiecutter.package_name}} = python.pkgs.buildPython{{cookiecutter.app_or_pkg.capitalize()}} {
-        #     pname = "{{cookiecutter.package_name}}";
-        #     version = "{{cookiecutter.package_version}}";
+        #   G4ShapePredictor = python.pkgs.buildPythonApplication {
+        #     pname = "G4ShapePredictor";
+        #     version = "1.0.0";
         #     pyproject = true;
         #     propagatedBuildInputs = with python.pkgs; [ setuptools ];
         #     src = ./.;
         #   };
-        #   default = {{cookiecutter.package_name}};
+        #   default = G4ShapePredictor;
         # };
         # apps = rec {
         #   {{cookiecutter.package_name}} = flake-utils.lib.mkApp {
@@ -38,35 +54,23 @@
         # };
         devShells = {
           default = pkgs.mkShell {
-            packages =
-              let
-                pysimplegui =
-                  with python.pkgs;
-                  buildPythonPackage {
-                    pname = "PySimpleGUI";
-                    version = "4.60.5";
-                    src = fetchTarball {
-                      url = "https://deleted-packages.pypimirror.stablebuild.com/pysimplegui/PySimpleGUI-4.60.5.tar.gz?expires=1749609803478&signature=1d086905c6dabbd415dec36054f1c41d23f4f56b076c8d797a73fbaa643ce193";
-                      sha256 = "1jpgighmkc3pw5n0g5i6cdjyczj5yp6h0yrmiwjbm3myhsf3rlrf";
-                    };
-                    doCheck = false;
-                  };
-              in
-              [
-                (python.withPackages (
-                  pypkgs: with pypkgs; [
-                    pysimplegui
-                    tkinter
-                    biopython
-                    pandas
-                    numpy
-                    scikit-learn
-                    lightgbm
-                    xgboost
-                    # catboost # broken on darwin
-                  ]
-                ))
-              ];
+            packages = [
+              (python.withPackages (
+                pypkgs: with pypkgs; [
+                  pysimplegui
+                  tkinter
+                  biopython
+                  pandas
+                  numpy
+                  scikit-learn
+                  lightgbm
+                  xgboost
+                  # catboost # broken on darwin
+
+                  ipython
+                ]
+              ))
+            ];
           };
         };
       }
